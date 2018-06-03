2524c88ccfb4bc0911e60a836ce3060f52ce9e9d
Joseph Ledford <jcl240@georgetown.edu>
2018-06-03 15:38:38 +0100

    Finally got tiles to fit board. board image 672x700, tiles 672x705

--- src/java/game/boardGUI.java
+++ src/java/game/boardGUI.java
@@ -1,24 +1,28 @@
 import javax.swing.*;
 import java.awt.*;
 
+import static java.awt.GridBagConstraints.BOTH;
+
 public class boardGUI {
     private JFrame frame = new JFrame();
     private JPanel mainPanel = new JPanel();
     private JButton[][] tiles = new JButton[24][25];
     private JPanel tilePanel = new JPanel();
     private JPanel backgroundPanel = new JPanel();
+    private GridBagConstraints c = new GridBagConstraints();
 
     public boardGUI() {
         frame.add(mainPanel);
 
-        mainPanel.setPreferredSize(new Dimension(740,700));
+        mainPanel.setPreferredSize(new Dimension(672,705));
         mainPanel.setLayout(new OverlayLayout(mainPanel));
+        frame.setResizable(false);
+        tilePanel.setLayout(new GridBagLayout());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
 
         mainPanel.add(tilePanel,0);
-        tilePanel.setLayout(new GridLayout(25,24));
         mainPanel.add(backgroundPanel,-1);
 
 
@@ -40,13 +44,17 @@ public class boardGUI {
 
     private void addTiles() {
         tilePanel.setOpaque(false);
+        c.fill = BOTH;
+        c.weighty=c.weightx=1;
         for(int y = 0; y < 25; y++){
             for(int x = 0; x < 24; x++){
+                c.gridx=x;
+                c.gridy=y;
                 tiles[x][y] = new JButton();
                 tiles[x][y].setOpaque(false);
                 tiles[x][y].setContentAreaFilled(false);
                 //tiles[x][y].setBorder( null );
-                tilePanel.add(tiles[x][y]);
+                tilePanel.add(tiles[x][y], c);
             }
         }
     }
