6670e24367851f37a6ebef1e5959c9fcdf99167b
Joseph Ledford <jcl240@georgetown.edu>
2018-06-09 11:47:52 +0100

    Put previous card commits into new method initializeCards()

--- src/java/game/Cluedo.java
+++ src/java/game/Cluedo.java
@@ -2,15 +2,23 @@ import java.util.Arrays;
 
 public class Cluedo {
 
+    public Card[] deck;
+    public Card[] envelope;
+    public Card[] faceUpCards;
+
     public Cluedo() {
-        Card[] deck = Card.makeCards();
-        Card[] envelope = Card.makeEnvelope(deck);
-        deck = Card.removeCards(envelope,deck);
-        deck = Card.shuffle(deck);
-        Card[] faceUp = new Card[]{deck[0], deck[1]};
-        deck = Arrays.copyOfRange(deck,2,deck.length);
+        initializeCards();
         Board board = new Board();
         boardGUI boardGUI = new boardGUI(board.getTiles());
+    }
+
+    public void initializeCards(){
+        deck = Card.makeCards();
+        envelope = Card.makeEnvelope(deck);
+        deck = Card.removeCards(envelope,deck);
+        deck = Card.shuffle(deck);
+        faceUpCards = new Card[]{deck[0], deck[1]};
+        deck = Arrays.copyOfRange(deck,2,deck.length);
     }
 
     public static void main(String[] args) {
