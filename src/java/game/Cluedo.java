0661b890403b7c2e5166fa0a20e4869ffe1f272e
Joseph Ledford <jcl240@georgetown.edu>
2018-06-09 11:31:02 +0100

    Select two face up cards then remove them from deck

--- src/java/game/Cluedo.java
+++ src/java/game/Cluedo.java
@@ -1,9 +1,14 @@
+import java.util.Arrays;
+
 public class Cluedo {
 
     public Cluedo() {
         Card[] deck = Card.makeCards();
         Card[] envelope = Card.makeEnvelope(deck);
         deck = Card.removeCards(envelope,deck);
+        deck = Card.shuffle(deck);
+        Card[] faceUp = new Card[]{deck[0], deck[1]};
+        deck = Arrays.copyOfRange(deck,2,deck.length);
         Board board = new Board();
         boardGUI boardGUI = new boardGUI(board.getTiles());
     }
